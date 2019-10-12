681b9e1190be02705cc2cec63ade39e6522e29b5
Davide57 <davide5713@gmail.com>
2019-10-12 18:05:37 +0200

    Add queries with args

--- Task0/src/main/java/workinggroup/task0/ConnectionManager.java
+++ Task0/src/main/java/workinggroup/task0/ConnectionManager.java
@@ -2,6 +2,7 @@ package workinggroup.task0;
  
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -38,28 +39,32 @@ public class ConnectionManager {
         return true;
     }
     /* Creates an ArrayList of the Object that needs to be returned as a result of the query */
-    public ArrayList <Object> worker(String query) throws SQLException{
-        
-        Statement stmt = conn.createStatement();
-        stmt.execute(query);                                    
-        ResultSet rs = stmt.getResultSet();   
-        
-        ArrayList<Object> result = new ArrayList<>();
-        Object o;
-                        
-        while(rs.next()){
-            /* TODO extract data here e mettere nella arraylist*/
-            o = chooseType(query, rs);
-            result.add(o);
-            System.out.print("line :  ");
-            //System.out.print(rs.getString("title"));
-            System.out.println(result); //stampa tutto, dividere le righe
-            //System.out.print("\n");
+    public ArrayList <Object> worker(String query, Object[] args) throws SQLException{
+        if(args == null){
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);                                    
+            ResultSet rs = stmt.getResultSet();  
+      
+            ArrayList<Object> result = new ArrayList<>();
+            Object o;
+
+            while(rs.next()){
+                o = chooseType(query, rs);
+                result.add(o);
+                System.out.print("line :  ");
+                System.out.println(result); 
+            }
+
+            rs.close();
+            stmt.close();
+            return result; 
+        }else{
+            PreparedStatement ps = conn.prepareStatement(query);
+            for(int i=0; i<args.length; i++){
+                ps.setObject(i + 1,args[i]);
+            }
+            ps.executeUpdate();
+            return null;
         }
-             
-        rs.close();
-        stmt.close();
-        //result.clear(); consigliabile svuotarla da qualche parte invece di aspettare il garbage collector?
-        return result;  // TODO: ritornare la lista giusta 
     }
 }
--- Task0/src/main/java/workinggroup/task0/DatabaseManager.java
+++ Task0/src/main/java/workinggroup/task0/DatabaseManager.java
@@ -16,27 +16,36 @@ public class DatabaseManager {
     public static final String SELECT_ALL_BOOKS = "SELECT * FROM  " + BOOK_TABLE;
     public static final String SELECT_ALL_PUBLISHERS = "SELECT * FROM " + PUBLISHER_TABLE;
     public static final String SELECT_ALL_AUTHORS = "SELECT * FROM  " + AUTHOR_TABLE;
-    /*private static final String SELECT_ALL_BOOKS_BY_AUTHOR = "SELECT title, firstName, lastName  FROM" + BOOK_TABLE + "JOIN"
-            + AUTHOR_TABLE + "ON idAUTHOR = AUTHOR_id";*/
-    public static final String INSERT = " INSERT INTO" + BOOK_TABLE;
+    
+    public static final String UPDATE_BOOK = "UPDATE " + BOOK_TABLE + "SET quantity = ";
+    public static final String SELECT_ALL_BOOKS_BY_AUTHOR = "SELECT title, firstName, lastName  FROM" + BOOK_TABLE + " INNER JOIN "
+            + AUTHOR_TABLE + " ON idAUTHOR = ?";
+    public static final String SELECT_ALL_BOOKS_BY_PUBLISHER = "SELECT title, firstName, lastName  FROM" + BOOK_TABLE + " INNER JOIN "
+            + AUTHOR_TABLE + " ON idPUBLISHER = ?";
+    public static final String INSERT_BOOK = "INSERT INTO" + BOOK_TABLE + " VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+    public static final String DELETE_BOOK = "DELETE FROM " + BOOK_TABLE + " WHERE id = ?";  
+    
     
     /* Handles commands inserted by the user via gui and launches the respective routine */
-    public void commandManager(ConnectionManager connManager, String cmd) throws SQLException{  //forse non serve
+    public void commandManager(ConnectionManager connManager, String cmd, Object[] args) throws SQLException{  //forse non serve
         switch(cmd){
             case SELECT_ALL_BOOKS: 
-                connManager.worker(SELECT_ALL_BOOKS);
+                connManager.worker(SELECT_ALL_BOOKS, null);
             break;
             case SELECT_ALL_PUBLISHERS: 
-                connManager.worker(SELECT_ALL_PUBLISHERS);
+                connManager.worker(SELECT_ALL_PUBLISHERS, null);
             break;
             case SELECT_ALL_AUTHORS: 
-                connManager.worker(SELECT_ALL_AUTHORS);
+                connManager.worker(SELECT_ALL_AUTHORS, null);
             break;
-            /*case INSERT:
-                insertBook(args);
-                break;*/
-            /*default: //errore
-                System.out.println("Wrong command");*/
+            case INSERT_BOOK:
+                connManager.worker(INSERT_BOOK, args);
+                break;
+            case DELETE_BOOK:
+                connManager.worker(DELETE_BOOK, args);
+                break;
+            default: 
+                System.out.println("Wrong command");
         }
     }
     
@@ -55,7 +64,7 @@ public class DatabaseManager {
         try {
             connManager.connectionStart();
             //commandManager(connManager, query);
-            connManager.worker(query);
+            connManager.worker(query,null);
             connManager.connectionClose();
         } catch(SQLException ex){
             System.out.println("SQLException: " + ex.getMessage());
