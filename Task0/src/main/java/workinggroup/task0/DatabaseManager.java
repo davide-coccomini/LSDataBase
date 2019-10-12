7a124ee7446fac4939161afb7f62b75ee42d9c03
Davide57 <davide5713@gmail.com>
2019-10-12 19:45:17 +0200

    Fix syntax error in query

--- Task0/src/main/java/workinggroup/task0/DatabaseManager.java
+++ Task0/src/main/java/workinggroup/task0/DatabaseManager.java
@@ -20,8 +20,8 @@ public class DatabaseManager {
     public static final String UPDATE_BOOK = "UPDATE " + BOOK_TABLE + "SET quantity = ";
     public static final String SELECT_ALL_BOOKS_BY_AUTHOR = "SELECT B.title, A.firstName, A.lastName  FROM " + BOOK_TABLE + " B NATURAL JOIN "
             + AUTHOR_TABLE + " A WHERE A.idAUTHOR = ?";
-    public static final String SELECT_ALL_BOOKS_BY_PUBLISHER = "SELECT title, firstName, lastName  FROM" + BOOK_TABLE + " B NATURAL JOIN "
-            + AUTHOR_TABLE + " A WHERE P.idPUBLISHER = ?";
+    public static final String SELECT_ALL_BOOKS_BY_PUBLISHER = "SELECT B.title, P.name  FROM " + BOOK_TABLE + " B NATURAL JOIN "
+            + PUBLISHER_TABLE + " P WHERE P.idPUBLISHER = ?";
     public static final String INSERT_BOOK = "INSERT INTO" + BOOK_TABLE + " VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
     public static final String DELETE_BOOK = "DELETE FROM " + BOOK_TABLE + " WHERE id = ?";  
     
