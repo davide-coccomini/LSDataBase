f71d9c8ad303025f65cc9f01193d051dc0020a8a
Davide57 <davide5713@gmail.com>
2019-10-27 01:17:58 +0200

    Center delete buttons

--- Task1/src/main/java/workinggroup/task1/UIController.java
+++ Task1/src/main/java/workinggroup/task1/UIController.java
@@ -10,6 +10,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
@@ -244,6 +245,7 @@ public class UIController
                         }       
                     }
                 };
+                cell.setAlignment(Pos.CENTER);
                 return cell;
             }
         };
@@ -323,6 +325,7 @@ public class UIController
                         }       
                     }
                 };
+                cell.setAlignment(Pos.CENTER);
                 return cell;
             }
         };
@@ -493,7 +496,7 @@ public class UIController
         
         final HBox hbox = new HBox();
         hbox.setSpacing(5);
-        hbox.setPadding(new Insets(10, 0, 0, 10));
+        hbox.setPadding(new Insets(0, 0, 0, 10));
       //  hbox.SimpleStringProperty("row");
         
       
